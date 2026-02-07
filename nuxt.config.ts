b18420a62e3b8d4851d5c36c01d33f48cdd4d1e4
ennersmai <mysweetieai@gmail.com>
2026-02-07 23:25:05 +0200

    Add revenueCatApiKey to runtimeConfig for mobile builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,6 +59,7 @@ export default defineNuxtConfig({
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+      revenueCatApiKey: process.env.NUXT_PUBLIC_REVENUECAT_API_KEY || '',
     }
   },
 
